83d2ec2c306d6843e6d833af9af02b40ff890bf0
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2024-04-15 20:27:25 +0200

    refbox_conn: properly mirror

--- src/refbox_connector.cpp
+++ src/refbox_connector.cpp
@@ -73,17 +73,17 @@ void RefboxConnector::mirror_mps(const MPS &mps) {
     } else {
       mirrored_name[0] = 'C';
     }
+    float threshold = 0.01f;
     float mirrored_rot = 0;
-    if (mps.angle == 0 || mps.angle == M_PI) {
+    if ((mps.angle < threshold) || (std::abs(mps.angle - M_PI) < threshold)) {
       mirrored_rot = mps.angle + M_PI;
-    } else if (mps.angle == 45 * M_PI / 180. ||
-               mps.angle == 135 * M_PI / 180.) {
-      mirrored_rot = mps.angle + M_PI / 2;
-    } else if (mps.angle == 90 * M_PI / 180. ||
-               mps.angle == 270 * M_PI / 180.) {
+    } else if (std::abs(mps.angle - 45 * M_PI / 180.) < threshold ||
+               std::abs(mps.angle - 135 * M_PI / 180.) < threshold) {
+      mirrored_rot = mps.angle + M_PI / 2.;
+    } else if (std::abs(mps.angle - 90 * M_PI / 180.) < threshold ||
+               std::abs(mps.angle - 270 * M_PI / 180.) < threshold) {
       mirrored_rot = mps.angle;
     } else {
-      printf("THIS SHOULD NEVER HAPPEN");
       fflush(stdout);
     }
 
